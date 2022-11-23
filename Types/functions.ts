77c4255fa851b0e6013152273c9cc843c3405db2
OpenYong <fkszm3@gmail.com>
2022-11-23 12:24:50 +0900

    3-18,19 More On Function Parameter Annotations

--- Types/functions.ts
+++ Types/functions.ts
@@ -12,3 +12,16 @@ function greet(person: string) {
 }
 
 greet("Yong");
+
+// 18. More On Function Parameter Annotations
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
+// 인자의 개수에 따라 혹인 인자의 타입에 따라 에러를 발생시킨다.
+//  doSomething("Yong"); // 인자 개수가 맞지 않음,
+//  doSomething("Yong",20, true, false); // 인자 개수가 맞지 않음,
+
+// 19. Working With Default Parameters
+function greeting(person: string = "누구세요") {
+  console.log(`안녕하세요, ${person}님`);
+}
+
