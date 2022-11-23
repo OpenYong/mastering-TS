fb657d4481092fc3f7272256f1e5265036b852d0
OpenYong <fkszm3@gmail.com>
2022-11-23 10:59:03 +0900

    3-13 Compiling TypeScript

---
+++ Types/variables.js
@@ -0,0 +1,9 @@
+var movieTitle = "Batman";
+movieTitle = "Arrival";
+// movieTitle = 9; // TS 오류
+movieTitle.toUpperCase();
+var myAge = 10;
+myAge += 1;
+// myAge = "열한살"; // TS 오류
+var gameOver = false;
+// gameOver = "true"; // TS 오류
