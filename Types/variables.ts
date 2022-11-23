a2202f5135a76c01b302fc4a39c5642a58246e34
OpenYong <fkszm3@gmail.com>
2022-11-23 12:07:53 +0900

    3-16. Delayed Initialization & Implicit Any

--- Types/variables.ts
+++ Types/variables.ts
@@ -22,3 +22,20 @@ thing = false;
 thing();
 thing.toUpperCase();
 
+const movies = ["Arrival", "Batman", "Avatar"];
+// let foundMovie: string;
+let foundMovie;
+
+for (let movie of movies) {
+  if (movie === "Batman") {
+    foundMovie = "Batman";
+  }
+}
+
+foundMovie(); // foundMovie가 암묵적으로 any가 때문에 TS가 에러를 발생시키지 않음.
+foundMovie = 1;
+foundMovie.asdasd();
+
+// 암묵적으로 any type이 될 수 있는 경우가 있다.
+// 변수만 선언이 되었을 경우 any로 type 지정이 되는데,
+// 이 경우에 TS가 개발자의 의도에 맞게 에러를 발생시키 못하기 때문에 주의가 필요하다.
