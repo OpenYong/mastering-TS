b29f83c99fa1e9633f8d806fa735e843a05abd81
OpenYong <fkszm3@gmail.com>
2022-11-23 10:53:00 +0900

    3-12 Working With Numbers & Booleans

--- Types/variables.ts
+++ Types/variables.ts
@@ -1,4 +1,11 @@
 let movieTitle: string = "Batman";
 movieTitle = "Arrival";
-movieTitle = 9;
+// movieTitle = 9; // TS 오류
 movieTitle.toUpperCase();
+
+let myAge: number = 10;
+myAge += 1;
+// myAge = "열한살"; // TS 오류
+
+let gameOver: boolean = false;
+// gameOver = "true"; // TS 오류
