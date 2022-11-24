0117882742c615a4af2632dcb69d0e8c0f05816c
OpenYong <fkszm3@gmail.com>
2022-11-24 18:22:26 +0900

    Section 10. Compiler

---
+++ configuringTS/.dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const dog = {
+    name: "ABC",
+    breed: "불독",
+    speed: 15,
+};
---
+++ configuringTS/index.ts
@@ -0,0 +1,11 @@
+interface Dog {
+  name: string;
+  breed: string;
+  speed: number;
+}
+
+const dog: Dog = {
+  name: "ABC",
+  breed: "불독",
+  speed: 15,
+};
