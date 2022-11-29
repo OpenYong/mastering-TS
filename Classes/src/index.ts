24add978557480cbf2eeba67310db6539e46ece4
OpenYong <fkszm3@gmail.com>
2022-11-29 15:07:10 +0900

    13-87 Class Fields In TypeScript

--- Classes/src/index.ts
+++ Classes/src/index.ts
@@ -1,8 +1,8 @@
 class Player {
-  // 타입스크립트에서는 클래스의 프로퍼티 타입을 지정해주어야한다.
-  // 다른 방법도 있음.
   first: string;
   last: string;
+  scroe: number = 0; // number를 지정해주지 않아도 되지만 명확하게 했다.
+
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
