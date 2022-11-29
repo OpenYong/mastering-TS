5f2cfb6ea769c8a961b43730e034f1d0becc06e7
OpenYong <fkszm3@gmail.com>
2022-11-29 15:41:55 +0900

    13-90. The private Modifier

--- Classes/src/index.ts
+++ Classes/src/index.ts
@@ -1,13 +1,20 @@
 class Player {
   public readonly first: string;
   public readonly last: string;
-  public scroe: number = 0; // number를 지정해주지 않아도 되지만 명확하게 했다.
+  private scroe: number = 0; // number를 지정해주지 않아도 되지만 명확하게 했다.
 
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
+
+    this.privateMethod(); // Class 내부에서만 사용이 가능하다.
+  }
+
+  private privateMethod(): void {
+    console.log("프라이빗");
   }
 }
 
 const player1 = new Player("Yong", "LEE");
-player1.last;
+player1.scroe; // private에 바로 접근할 수 없다.
+player1.privateMethod(); // private에 바로 접근할 수 없다.
