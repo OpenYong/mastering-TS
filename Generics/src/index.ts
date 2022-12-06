435d5f4da61a574bd4db20121b6b95c8bdb3c3ae
Yong <fkszm3@gmail.com>
2022-12-06 22:35:51 +0900

    14 - 97. Another Example Of A Built-In Generic

---
+++ Generics/src/index.ts
@@ -0,0 +1,18 @@
+const nums: number[] = [];
+const nums2: Array<number> = [];
+const colors: Array<string> = [];
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+inputEl.value = "바보바보";
+
+const btn = document.querySelector<HTMLButtonElement>(".btn")!;
