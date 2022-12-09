68afa99a68fd664febe5437b5a78491f0ceb34b6
Yong <fkszm3@gmail.com>
2022-12-09 09:41:30 +0900

    15-107. Truthiness Guards

--- TypeNarrowing/index.ts
+++ TypeNarrowing/index.ts
@@ -1,6 +1,28 @@
+// 106. Typeof Narrowing:
 function triple(age: string | number) {
   if (typeof age === "string") {
     return age.repeat(3);
   }
   return age * 3;
 }
+
+// 107. Truthiness Guards
+const el = document.getElementById("idk");
+if (el) {
+  el;
+} else {
+  el;
+}
+
+// 107. Truthiness Narrowing:
+const printLetters = (word?: string) => {
+  if (word) {
+    for (let char of word) {
+      console.log(char);
+    }
+  } else {
+    // word가 반드시 undefined가 되는 것이 아니다.
+    // 빈 문자열도 false가 될 수 있음.
+    console.log("매개변수를 전달하지 않았음!");
+  }
+};
