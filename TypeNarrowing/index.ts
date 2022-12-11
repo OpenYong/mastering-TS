7d9ca89b5e22beb308b972f4e5bb7aea0770eb87
Yong <fkszm3@gmail.com>
2022-12-11 10:41:13 +0900

    15-111. Type Predicates

--- TypeNarrowing/index.ts
+++ TypeNarrowing/index.ts
@@ -83,3 +83,27 @@ function printName(entity: User | Company) {
     entity;
   }
 }
+
+// 111. Type Predicates
+interface Cat {
+  name: string;
+  numLives: number;
+}
+interface Dog {
+  name: string;
+  breed: string;
+}
+
+function isCat(animal: Cat | Dog): animal is Cat {
+  return (animal as Cat).numLives !== undefined;
+}
+
+function makeNoise(animal: Cat | Dog): string {
+  if (isCat(animal)) {
+    animal;
+    return "Meow";
+  } else {
+    animal;
+    return "Woof!";
+  }
+}
